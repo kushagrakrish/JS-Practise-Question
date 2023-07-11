c3fe41c55afac2025e59706e660b9497702aaf73
@kushagra24 <krishna619kk@gmail.com>
2023-07-11 10:51:00 +0530

    All JS methods

--- method.js
+++ method.js
@@ -43,7 +43,7 @@ const greaterMass100 = characters?.filter((character) => {
 });
 console.log(greaterMass100);
 
-// JS array methods returns a new array from the existing array and the returned array should consist only those elements which satisfy the given condition set by argument
+// JS filter array methods returns a new array from the existing array and the returned array should consist only those elements which satisfy the given condition set by argument
 
 // Get all male charac
 const allMaleCharacter = characters?.filter((character) => {
@@ -83,6 +83,7 @@ const maxRecord = characters?.map((character) => {
 console.log(maxRecord);
 
 // Some method in JS
+// The Javascript arr.some() method checks whether at least one of the elements of the array satisfies the condition checked by the argument method.
 
 const oneMaleCharc = characters?.some((character) => {
   return character.gender === "male";
@@ -94,3 +95,39 @@ const oneVlueEye = characters?.some((character) => {
   return character.eye_color === "blue";
 });
 console.log(oneVlueEye);
+
+// JS Sort method
+const byMass = characters?.sort((a, b) => {
+  return a.mass - b.mass;
+});
+console.log(byMass);
+
+// Sort by name
+const byName = characters?.sort((a, b) => {
+  if (a.name < b.name) return 1;
+  else 1;
+});
+console.log(byName);
+
+const byGender = characters?.sort((a, b) => {
+  if (a.gender === "female") return -1;
+  else -1;
+});
+console.log(byGender);
+
+// Reduce Method JS
+// calculate total mass
+const totalMass = characters?.reduce((acc, cur) => {
+  return acc + cur.mass;
+}, 0);
+
+console.log(totalMass);
+
+// Total Name character
+const totalName = characters?.reduce((acc, cur) => {
+  return acc + cur.name.length;
+}, 0);
+
+console.log(totalName);
+
+// The Javascript arr.reduce() method in JavaScript is used to reduce the array to a single value and executes a provided function for each value of the array (from left to right) and the return value of the function is stored in an accumulator.
