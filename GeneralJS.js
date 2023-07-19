b2e0293874dc9bf9c3d914f76359a363bc72419d
@kushagra24 <krishna619kk@gmail.com>
2023-07-19 22:24:12 +0530

    JS Cllback func

---
+++ GeneralJS.js
@@ -0,0 +1,12 @@
+function callbackFunction() {
+  console.log("Hello Callback");
+}
+
+setTimeout(callbackFunction, 4000);
+
+const helloWorld = () => {
+  setTimeout(() => {
+    console.log("first");
+  }, 2000);
+};
+helloWorld();
